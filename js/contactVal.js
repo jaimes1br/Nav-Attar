5376a31e3c588aaa42a3cc39c35a800ae1fc9225
Ilán Sánchez <ilancsanchezor@gmail.com>
2021-11-03 17:48:01 -0600

    formulario validar

--- js/contactVal.js
+++ js/contactVal.js
@@ -1,12 +1,35 @@
-const nombre = document.getElementById("nombre");
-const correo = document.getElementById("correo");
-const asunto = document.getElementById("asunto");
-const mensaje = document.getElementById("mensaje");
-const form = document.getElementById("form");
+let nombre = document.getElementById("nombre");
+let email = document.getElementById("correo");
+let asunto = document.getElementById("asunto");
+let mensaje = document.getElementById("mensaje");
+let form = document.getElementById("form");
 
-form.addEventlistener("submit", e=>{
-  e.preventDefault()
-  if(nombre.ariaValueMax.length <6){
-    alert("Nombre corto")
-  }
+
+form.addEventListener("submit", e=>{
+ e.preventDefault()
+ 
+ let Temail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+if(nombre.value.length <= 2 || nombre.value.length == 0){
+    alert("nombre invalido") 
+}
+if(!Temail.test(email.value) || Temail.value.length==0){
+   
+  alert("Correo invalido")
+}
+
+if(asunto.value.length >= 50 || asunto.value.length == 0){
+
+    alert("asunto invalido")
+
+}
+
+if(mensaje.value.length >= 150 || mensaje.value.length==0){
+
+   alert("mensaje invalido") 
+
+}
 })
+
+
+
