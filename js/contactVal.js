38a7265ebdde59fcb7fa3a49ff1f017aa790995d
Erik Daniel <erikrame69@gmail.com>
2021-11-03 14:16:28 -0600

    create function

--- js/contactVal.js
+++ js/contactVal.js
@@ -1,4 +1,24 @@
 
-function validation (){
+let valName = document.getElementById("nombre");
+valName.addEventListener("click", function(e){
+    let validContac;
+    function validContac(){
+
+        const nombre;
     
-}
+        nombre=document.getElementById("nombre").value;
+    
+        if(nombre == "")   
+    {
+        div_error.innerHTML="El campo del nombre esta vacío";
+        return false;
+        }else if (nombre.length>50 ){
+            div_error.innerHTML="El campo del nombre es muy large"; 
+        return false;
+        }
+        else{
+            div_error.innerHTML = "Campo validado";
+        }
+    }
+});
+
