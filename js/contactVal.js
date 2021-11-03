2e67459e63843a32531c7f14e45b1155c43b63fc
Ilán Sánchez <ilancsanchezor@gmail.com>
2021-11-03 14:14:52 -0600

    formulario

--- js/contactVal.js
+++ js/contactVal.js
@@ -1,4 +1,12 @@
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const asunto = document.getElementById("asunto");
+const mensaje = document.getElementById("mensaje");
+const form = document.getElementById("form");
 
-function validation (){
-    
-}
+form.addEventlistener("submit", e=>{
+  e.preventDefault()
+  if(nombre.ariaValueMax.length <6){
+    alert("Nombre corto")
+  }
+})
