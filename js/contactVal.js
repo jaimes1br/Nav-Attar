72e3891c81f847c71c491a755647c508d97253cb
Ilán Sánchez <ilancsanchezor@gmail.com>
2021-11-03 19:32:25 -0600

    Formulario

--- js/contactVal.js
+++ js/contactVal.js
@@ -1,28 +1,51 @@
+let nombre = document.getElementById("nombre");
+let email = document.getElementById("correo");
+let asunto = document.getElementById("asunto");
+let mensaje = document.getElementById("mensaje");
+let form = document.getElementById("form");
+let alertaDiv = document.getElementById("alerta");
+
 form.addEventListener("submit", e=>{
  e.preventDefault()
- 
+  
+ let valido = false
+ let alerta = ""
+ alertaDiv.innerHTML = ""
  let Temail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 if(nombre.value.length <= 2 || nombre.value.length == 0){
-    alert("nombre invalido") 
+    alerta += `Nombre no valido <br>`
+    valido = true
 }
-if(!Temail.test(email.value) || Temail.value.length==0){
+if(!Temail.test(email.value) || email.value.length==0){
    
-  alert("Correo invalido")
+  alerta += `Email no valido <br>`
+  valido = true
 }
 
 if(asunto.value.length >= 50 || asunto.value.length == 0){
 
-    alert("asunto invalido")
-
+  alerta += `Asunto no valido <br>`
+  valido = true
 }
 
 if(mensaje.value.length >= 150 || mensaje.value.length==0){
 
-   alert("mensaje invalido") 
+  alerta += `Mensaje no valido <br>`
+  valido = true
 
 }
+
+if(valido){
+
+  alertaDiv.innerHTML = alerta
+}else{
+
+  alertaDiv.innerHTML = "Enviado"
+}
+
 })
+
 
 
 
