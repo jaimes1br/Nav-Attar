ba36c16b3561b7aaa2b49acdab927ad6251ecbdf
Ilán Sánchez <ilancsanchezor@gmail.com>
2021-11-03 20:19:31 -0600

    Correo en formulario

--- js/contactVal.js
+++ js/contactVal.js
@@ -3,14 +3,14 @@ let email = document.getElementById("correo");
 let asunto = document.getElementById("asunto");
 let mensaje = document.getElementById("mensaje");
 let form = document.getElementById("form");
-let alertaDiv = document.getElementById("alerta");
+let alertas = document.getElementById("alerta");
 
 form.addEventListener("submit", e=>{
  e.preventDefault()
   
  let valido = false
  let alerta = ""
- alertaDiv.innerHTML = ""
+ alertas.innerHTML = ""
  let Temail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 if(nombre.value.length <= 2 || nombre.value.length == 0){
@@ -38,21 +38,13 @@ if(mensaje.value.length >= 150 || mensaje.value.length==0){
 
 if(valido){
 
-  alertaDiv.innerHTML = ` 
-      <div class="alert alert-dark" role="alert"> 
-      ${alerta}
-        
-      </div>`;
+  alertas.innerHTML = alerta;
 
 }else{
 
-  var correo = document.createElement("a");
-  correo.href = `b1jaimes89@gmail.com?subject=${asunto.value}
-  &body=Nombre:\n\n ${nombre.value}
-  \n\nMensaje: \n\n ${mensaje.value}
-  \n\n Correo Cliente: ${correo.value}`;
-
-  correo.click();
+  var dir = document.createElement("a");
+        dir.href = `mailto:nav.attar.contact@gmail.com?subject=${asunto.value}&body=${nombre.value}%20%0D%0A %20%0D%0A${mensaje.value} %20%0D%0A %20%0D%0A %20%0D%0A Correo cliente: ${correo.value}`;
+        dir.click();
 }
 
 })
