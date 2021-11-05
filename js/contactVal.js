350be8b8c659e636de35608dec504542a83b12b2
Ilán Sánchez <ilancsanchezor@gmail.com>
2021-11-05 02:27:36 -0600

    Correccion formulario

--- js/contactVal.js
+++ js/contactVal.js
@@ -1,6 +1,6 @@
 let nombre = document.getElementById("nombre");
 let email = document.getElementById("correo");
-let asunto = document.getElementById("asunto");
+let tel = document.getElementById("tel");
 let mensaje = document.getElementById("mensaje");
 let form = document.getElementById("form");
 let alertas = document.getElementById("alerta");
@@ -11,27 +11,29 @@ form.addEventListener("submit", e=>{
  let valido = false
  let alerta = ""
  alertas.innerHTML = ""
- let Temail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+ let temail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+ let tnombre = /^[a-zA-Z ]*$/
+ let ttel =/^[0-9]*$/
 
-  if(nombre.value.length <= 2 || nombre.value.length == 0){
-      alerta += `Nombre no valido <br>`
+  if(!tnombre.test(nombre.value) || nombre.value.length <= 2 || nombre.value.length == 0){
+      alerta += `¡Nombre no valido! <br>`
       valido = true
   }
-  if(!Temail.test(email.value) || email.value.length==0){
+  if(!temail.test(email.value) || email.value.length==0 || email.value.length >= 320){
     
-    alerta += `Email no valido <br>`
+    alerta += `¡Email no valido! <br>`
     valido = true
   }
 
-  if(asunto.value.length >= 50 || asunto.value.length == 0){
+  if(!ttel.test(tel.value) || tel.value.length < 10 || tel.value.length == 0 || tel.value.length >= 12){
 
-    alerta += `Asunto no valido <br>`
+    alerta += `¡Teléfono no valido! <br>`
     valido = true
   }
 
-  if(mensaje.value.length >= 150 || mensaje.value.length==0){
+  if(mensaje.value.length >= 150 || mensaje.value.length == 0){
 
-    alerta += `Mensaje no valido <br>`
+    alerta += `¡Mensaje no valido! <br>`
     valido = true
 
   }
@@ -40,13 +42,20 @@ form.addEventListener("submit", e=>{
 
     alertas.innerHTML = alerta
 
-  }else{
+  }
 
+  if(!valido){
+
+    alerta +=`¡Formulario Correcto!`
+    alertas.innerHTML = alerta;
     var dir = document.createElement("a");
-    dir.href = `mailto:nav.attar.contact@gmail.com?subject=${asunto.value}&body=${nombre.value}%20%0D%0A %20%0D%0A${mensaje.value} %20%0D%0A %20%0D%0A %20%0D%0A Correo cliente: ${email.value}`;
+    dir.href = `mailto:nav.attar.contact@gmail.com?&body=${nombre.value}%20%0D%0A %20%0D%0A${mensaje.value} %20%0D%0A %20%0D%0A %20%0D%0A Correo cliente: ${email.value} %20%0D%0A %20%0D%0A %20%0D%0A Teléfono cliente: ${tel.value}`;
     dir.click();
   }
 
+  // var dir = document.createElement("a");
+  // dir.href = `mailto:nav.attar.contact@gmail.com?subject=${asunto.value}&body=${nombre.value}%20%0D%0A %20%0D%0A${mensaje.value} %20%0D%0A %20%0D%0A %20%0D%0A Correo cliente: ${email.value}`;
+  // dir.click();
 })
 
 
