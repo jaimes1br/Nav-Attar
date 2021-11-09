4ef32b54341215612d559a21d2dc7ccd97985671
Erik Daniel <erikrame69@gmail.com>
2021-11-09 16:13:00 -0600

    Adición de las imagenes del producto

---
+++ js/productos.js
@@ -0,0 +1,7 @@
+addItem({
+    'nombre':'producto',
+    'tamaño':'25cm',
+    'categoria':'pendiente',
+    'precio':'$$',
+    'img':'Pendiente a insertar',
+    'descripción':'Descripcion pendiente a realizar'});
