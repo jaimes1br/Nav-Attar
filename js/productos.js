b47bf9a7bc847ea03854c6618e54fde3f23044e9
Erik Daniel <erikrame69@gmail.com>
2021-11-09 22:49:48 -0600

    Actualización de Productos

--- js/productos.js
+++ js/productos.js
@@ -1,7 +1,222 @@
-addItem({
-    'nombre':'producto',
-    'tamaño':'25cm',
-    'categoria':'pendiente',
-    'precio':'$$',
-    'img':'Pendiente a insertar',
-    'descripción':'Descripcion pendiente a realizar'});
+addItem[{
+    'id':'1',
+    'nombre':'Batman',
+    'tamaño':'20',
+    'categoria':'superheroe',
+    'precio':'200',
+    'img':'../img/muñequitos/batman.jpg',
+    'descripción':'Batman es un superheroe que te acompañara en todas tus aventuras, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'2',
+    'nombre':'Baby Yoda',
+    'tamaño':'20',
+    'categoria':'superheroe',
+    'precio':'350',
+    'img':'../img/muñequitos/bby.jpg',
+    'descripción':'Si cool quieres ser, Baby Yoda debes tener, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'3',
+    'nombre':'Carlitos',
+    'tamaño':'20',
+    'categoria':'caricaturas',
+    'precio':'650',
+    'img':'../img/muñequitos/carlitos.jpg',
+    'descripción':'¿Recuerdas los momentos en familia cuando veian juntos los Rugrats? Como olvidar al adorable Carlitos, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'4',
+    'nombre':'Coraline',
+    'tamaño':'22',
+    'categoria':'caricaturas',
+    'precio':'420',
+    'img':'../img/muñequitos/carol.jpg',
+    'descripción':'Muñequito de Coraline tejido, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'5',
+    'nombre':'Muñequita de Chinos',
+    'tamaño':'20',
+    'categoria':'personalizado',
+    'precio':'600',
+    'img':'../img/muñequitos/chinos.jpg',
+    'descripción':'Muñequita con mejillas sonrojadas y cabello chino, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'6',
+    'nombre':'Perrito personalizado',
+    'tamaño':'25',
+    'categoria':'personalizado',
+    'precio':'420',
+    'img':'../img/muñequitos/dog2.jpg',
+    'descripción':'Este es un perrito de un cliente, nos mando su foto y se personalizo el muñeco tejido, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'7',
+    'nombre':'Perrito personalizado bull terrier',
+    'tamaño':'25',
+    'categoria':'mascota',
+    'precio':'420',
+    'img':'../img/muñequitos/dog2.jpg',
+    'descripción':'Este es un perrito de un cliente, nos mando su foto y se personalizo el muñeco tejido, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'8',
+    'nombre':'Friend',
+    'tamaño':'18',
+    'categoria':'personalizado',
+    'precio':'200',
+    'img':'../img/muñequitos/friend.jpg',
+    'descripción':'Regala algo a tus seres queridos, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'9',
+    'nombre':'Erizo',
+    'tamaño':'15',
+    'categoria':'mascotas',
+    'precio':'130',
+    'img':'../img/muñequitos/erizo.jpg',
+    'descripción':'Recordemos a nuestras mascotas con algo pequeñito pero con mucho amor, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'10',
+    'nombre':'Arnols y Helga',
+    'tamaño':'20',
+    'categoria':'caricaturas',
+    'precio':'380',
+    'img':'../img/muñequitos/heyh.jpg',
+    'descripción':'¿Alguna vez amaste a alguien en secreto como Helga? No hay mejor presente que Arnold y Helga, los muñecos se venden por separado, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'11',
+    'nombre':'Homero Simpson',
+    'tamaño':'13',
+    'categoria':'caricatura',
+    'precio':'120',
+    'img':'../img/muñequitos/homero.jpg',
+    'descripción':'Podemos preguntar ¿Quién no ha visto The Simpsons? Homero J. Simpson sin duda es el personaje más comico de la serie, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'12',
+    'nombre':'Personajes de Harry Potter',
+    'tamaño':'15',
+    'categoria':'peliculas',
+    'precio':'200',
+    'img':'../img/muñequitos/hp.jpg',
+    'descripción':'El grupo de amigos más magico en el mundo, los muñecos se venden por separado y su precio individual es el que se indiva, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'id':'13',
+    'nombre':'Iron Maiden',
+    'tamaño':'15',
+    'categoria':'Celebridades',
+    'precio':'420',
+    'img':'../img/muñequitos/iron.jpg',
+    'descripción':'El regalo perfecto para los Iron Maiden Lover 💕, recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    },
+    {
+    'nombre':'',
+    'tamaño':'',
+    'categoria':'',
+    'precio':'',
+    'img':'../img/muñequitos',
+    'descripción':', recuerda que puedes personalizarlo a tu gusto'
+    }];
+
