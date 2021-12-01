fe8aabb82d37992b97445ebf2a89be3a94d8a80b
Erik Daniel <erikrame69@gmail.com>
2021-12-01 11:07:52 -0600

    Añadimos carpeta Java userproducts

---
+++ userproducts/src/main/java/com/navattar/userproducts/UserproductsApplication.java
@@ -0,0 +1,13 @@
+package com.navattar.userproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserproductsApplication.class, args);
+	}
+
+}
---
+++ userproducts/src/test/java/com/navattar/userproducts/UserproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navattar.userproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
