9bc8884e74456b29027987246b55e6e55103bdfd
Pablo PGP <pabloperezgilp@gmail.com>
2021-12-09 14:04:22 -0600

    Pruebas unitarias cliente

--- cliente_producto/src/test/java/com/navattar/cliente_producto/ClienteProductoApplicationTests.java
+++ cliente_producto/src/test/java/com/navattar/cliente_producto/ClienteProductoApplicationTests.java
@@ -22,7 +22,7 @@ class ClienteProductoApplicationTests {
 	//Añadimos dependencias
 	//comentamos los beans en la clase main
 	//creamos un nuevo Set setClearContrasena (para evitar que encripte dos veces)
-	
+
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -37,7 +37,7 @@ class ClienteProductoApplicationTests {
 		this.mockMvc.perform(get("/api/clientes/"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("@gmail.com")));
+				.andExpect(content().string(containsString(".com")));
 
 		this.mockMvc.perform(get("/api/clientes/1"))
 				.andDo(print())
@@ -46,15 +46,10 @@ class ClienteProductoApplicationTests {
 
 	}//TestGet
 
-	@Test
-	public void TestDelete() throws Exception{
-		this.mockMvc.perform( delete("/api/clientes")).andDo(print()).andExpect(status().isOk());
-	}//TestDelete
-
 
 	@Test
 	public void shouldReturn500Error() throws Exception{
-		this.mockMvc.perform(get("/api/clientes")).
+		this.mockMvc.perform(get("/api/clientes/8")).
 				andDo(print()).
 				andExpect(status().is5xxServerError());
 	}//shouldReturn500Error
@@ -62,10 +57,17 @@ class ClienteProductoApplicationTests {
 	@Test
 	public void TestPOST() throws Exception{
 		cliente _cliente = new cliente();
+		_cliente.setNombre("Eika");
 		_cliente.setCorreo_electronico("eflores@idr.com");
-		_cliente.setClearContrasena("Pa$$w0rd");
+		_cliente.setContrasena("Pa$$w0rd");
+		_cliente.setTelefono("5588996644");
 		this.mockMvc.perform(post("/api/clientes/").contentType(MediaType.APPLICATION_JSON).content(asJSONString(_cliente))).andExpect(status().isOk());
 	}//TestPOST
+
+	@Test
+	public void TestDelete() throws Exception{
+		this.mockMvc.perform( delete("/api/clientes/6")).andDo(print()).andExpect(status().isOk());
+	}//TestDelete
 
 	@Test
 	public void TestPUT () throws Exception {
